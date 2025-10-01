10b091b35b6c19f28283c061ee9d0388653ff02e
M307user <M307user@lab.uc-bcf.local>
2025-10-01 11:24:31 +0800

    HTDocS

---
+++ intro.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Introduction</title>
+</head>
+<body>
+    
+    <?php
+        $age="20";
+        $name="John";
+        $color="blue";
+
+        echo "Hi, I’m John, I am 20 years old, and my favorite color is blue.";
+    ?>
+    
+</body>
+</html>
